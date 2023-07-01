eac79a2b5e623bd0f3d76ac57ce0a6fc4c1cb1f3
Abuzar Mamedov <abuzar.mamedov@yandex.ru>
2023-07-01 03:49:33 +0300

    feat: add roboto font

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React, { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import App from "./App"
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './components/App/App'
+import './main.css'
 
-const rootNode = document.getElementById("root")
+const rootNode = document.getElementById('root')
 const root = createRoot(rootNode)
 root.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 )
