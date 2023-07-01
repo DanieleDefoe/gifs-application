ddabf9f227639527ef99067f34e3c3afc85d0127
Abuzar Mamedov <abuzar.mamedov@yandex.ru>
2023-07-01 19:15:12 +0300

    hotfix: remove search wrapper

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -17,32 +17,29 @@ export default function Form() {
   }
 
   return (
-    <section className="search">
-      <form className="form" onSubmit={onSubmit} noValidate>
-        <fieldset className="form__field">
-          <Input />
-          {searchError && (
-            <span className="form__input-error">{searchError}</span>
-          )}
-        </fieldset>
-        <ul className="form__buttons">
-          <li className="form__button-item">
-            <button
-              className="form__button form__button-close"
-              type="reset"
-              onClick={onReset}
-            />
-          </li>
-          <li className="form__button-item">
-            <button
-              className="form__button form__button-search"
-              type="submit"
-              disabled={!isValid}
-            />
-          </li>
-        </ul>
-      </form>
-      <Button />
-    </section>
+    <form className="form" onSubmit={onSubmit} noValidate>
+      <fieldset className="form__field">
+        <Input />
+        {searchError && (
+          <span className="form__input-error">{searchError}</span>
+        )}
+      </fieldset>
+      <ul className="form__buttons">
+        <li className="form__button-item">
+          <button
+            className="form__button form__button-close"
+            type="reset"
+            onClick={onReset}
+          />
+        </li>
+        <li className="form__button-item">
+          <button
+            className="form__button form__button-search"
+            type="submit"
+            disabled={!isValid}
+          />
+        </li>
+      </ul>
+    </form>
   )
 }
