786ec5f96a5a848bba7b88eea84ac89a6a02a561
Abuzar Mamedov <abuzar.mamedov@yandex.ru>
2023-07-01 15:57:38 +0300

    feat: add form validation hook

---
+++ src/hooks/useValidation.js
@@ -0,0 +1,31 @@
+import { useState, useCallback } from 'react'
+
+export function useValidation() {
+  const [values, setValues] = useState({})
+  const [errors, setErrors] = useState({})
+  const [isValid, setIsValid] = useState(false)
+
+  function handleChange(event) {
+    const { name, value } = event.target
+
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value
+    }))
+
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: value,
+    }))
+
+    setIsValid(event.target.closest('form').checkValidity())
+  }
+
+  const resetForm = useCallback((newValues = {}, newErrors = {}, newIsValid = false) => {
+    setValues(newValues)
+    setErrors(newErrors)
+    setIsValid(newIsValid)
+  }, [])
+
+  return { values, errors, isValid, handleChange, resetForm }
+}
