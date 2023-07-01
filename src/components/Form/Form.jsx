5d0cbeea06009aa6ae8a97cd1362a972c12f7192
BISWAP9 <116273370+BISWAP9@users.noreply.github.com>
2023-07-01 17:58:58 +0300

    feat: button show more

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,17 @@
+import Button from '../Button/Button';
 import Input from '../Input/Input';
 
 import './Form.css';
 
 export default function Form() {
   return (
-    <form className="form">
-      <Input />
-      <button className="form__button form__button-close"></button>
-      <button className="form__button form__button-search"></button>
-    </form>
+    <section className="search">
+      <form className="form">
+        <Input />
+        <button className="form__button form__button-close"></button>
+        <button className="form__button form__button-search"></button>
+      </form>
+      <Button />
+    </section>
   );
 }
