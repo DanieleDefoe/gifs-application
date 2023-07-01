145e16013393a7bd23b431d8d0cd875b7936f866
Abuzar Mamedov <abuzar.mamedov@yandex.ru>
2023-07-01 22:05:05 +0300

    feat: add loading state when fetching data

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,5 @@
-import { useSelector, useDispatch } from 'react-redux'
-
 import './Main.css'
 
 export default function Main({ children }) {
-  const dispatch = useDispatch()
-  useSelector((store) => {
-    console.log(store)
-  })
-
   return <main className="main">{children}</main>
 }
