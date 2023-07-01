fc2e7adbf046cc88981605c52ea9004c0cedb749
Abuzar Mamedov <abuzar.mamedov@yandex.ru>
2023-07-01 13:43:08 +0300

    feat: add redux store to the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './components/App/App'
+import store from './store/store'
 import './main.css'
 
 const rootNode = document.getElementById('root')
 const root = createRoot(rootNode)
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
