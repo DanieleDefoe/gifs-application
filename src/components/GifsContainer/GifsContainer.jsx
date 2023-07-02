c4e1fdfa046aa8246fb3a07a16ef4d5dbacac132
BISWAP9 <116273370+BISWAP9@users.noreply.github.com>
2023-07-02 12:03:54 +0300

    Pangination

--- src/components/GifsContainer/GifsContainer.jsx
+++ src/components/GifsContainer/GifsContainer.jsx
@@ -1,22 +1,20 @@
-import { useSelector } from 'react-redux'
-import Gif from '../Gif/Gif'
-import './GifsContainer.css'
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import Pangination from '../../Pangination/Pangination';
+import Gif from '../Gif/Gif';
+import './GifsContainer.css';
 
 export default function GifsContainer() {
-  const { data } = useSelector((store) => store.gifs)
+  const { data } = useSelector(store => store.gifs);
 
   return (
-    <section className="gifs-container">
-      {data.map(({ id, title, embed_url, username }) => {
-        return (
-          <Gif
-            key={id}
-            title={title}
-            embed_url={embed_url}
-            username={username}
-          />
-        )
-      })}
-    </section>
-  )
+    <Fragment>
+      <section className="gifs-container">
+        {data.map(({ id, title, embed_url, username }) => {
+          return <Gif key={id} title={title} embed_url={embed_url} username={username} />;
+        })}
+      </section>
+      <Pangination />
+    </Fragment>
+  );
 }
