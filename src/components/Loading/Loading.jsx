daecf06ad23db9d2acd5a1a969c4cd53867e72e9
BISWAP9 <116273370+BISWAP9@users.noreply.github.com>
2023-07-02 20:54:36 +0300

    3

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,5 +1,23 @@
-import './Loading.css'
+import './Loading.css';
 
 export default function Loading() {
-  return <p className="loading">Loading...</p>
+  return (
+    <p className="loading">
+      <div className="weight">
+        <div className="panda">
+          <div className="panda__contain">
+            <div className="panda__ears"></div>
+            <div className="panda__head">
+              <div className="panda__eyes"></div>
+              <div className="panda__nose"></div>
+            </div>
+          </div>
+          <div className="panda__arms"></div>
+          <div className="panda__body"></div>
+          <div className="panda__legs"></div>
+        </div>
+      </div>
+      <div className="panda__shadow"></div>
+    </p>
+  );
 }
