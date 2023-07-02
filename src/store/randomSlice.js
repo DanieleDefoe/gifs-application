e150b43a399baffba7fe240cb8408fcdac5b64f6
BISWAP9 <116273370+BISWAP9@users.noreply.github.com>
2023-07-02 13:28:57 +0300

    randomSliсes

---
+++ src/store/randomSlice.js
@@ -0,0 +1,32 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { BASE_URL, API_KEY } from '../utils/constants';
+
+export const getRandom = createAsyncThunk('random/getRandom', async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/random?api_key=${API_KEY}&limit=1`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const randomSlice = createSlice({
+  name: 'random',
+  initialState: { isLoading: null, data: [] },
+  reducers: {},
+  extraReducers: {
+    [getRandom.pending](state) {
+      state.isLoading = true;
+    },
+    [getRandom.fulfilled](state, action) {
+      state.data = action.payload.data;
+      state.isLoading = false;
+    },
+    [getRandom.rejected](state) {
+      state.isLoading = false;
+    }
+  }
+});
+
+export default randomSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import formSlice from './formSlice';
 import gifsSlice from './gifsSlice';
+import randomSlice from './randomSlice';
 import trendingsSlice from './trendingsSlice';
 
 const store = configureStore({
   reducer: {
     form: formSlice,
     gifs: gifsSlice,
-    trendings: trendingsSlice
+    trendings: trendingsSlice,
+    random: randomSlice
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
