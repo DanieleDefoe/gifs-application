8dcc27404e5d8322052eba69623bf2f233f14338
Abuzar Mamedov <abuzar.mamedov@yandex.ru>
2023-07-03 20:13:53 +0300

    feat: add pagination (beta) for search page

--- src/store/gifsSlice.js
+++ src/store/gifsSlice.js
@@ -1,9 +1,9 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { BASE_URL, API_KEY } from '../utils/constants';
 
-export const getSearchGifs = createAsyncThunk('gifs/getSearchGifs', async (query) => {
+export const getSearchGifs = createAsyncThunk('gifs/getSearchGifs', async ({ searchValue, offset }) => {
   try {
-    const response = await fetch(`${BASE_URL}/search?api_key=${API_KEY}&q=${query}&limit=9`);
+    const response = await fetch(`${BASE_URL}/search?api_key=${API_KEY}&q=${searchValue}&limit=9&offset=${offset}`);
     const data = await response.json();
     return data;
   } catch (error) {
@@ -13,13 +13,18 @@ export const getSearchGifs = createAsyncThunk('gifs/getSearchGifs', async (query
 
 const gifsSlice = createSlice({
   name: 'gifs',
-  initialState: { isLoading: null, data: [] },
-  reducers: {},
+  initialState: { isLoading: null, data: [], offset: 0 },
+  reducers: {
+    updateOffset(state) {
+      state.offset += 9
+    },
+  },
   extraReducers: {
     [getSearchGifs.pending](state) {
       state.isLoading = true;
     },
     [getSearchGifs.fulfilled](state, action) {
+      console.log(action.payload.pagination)
       state.data = action.payload.data;
       state.isLoading = false;
     },
@@ -29,4 +34,5 @@ const gifsSlice = createSlice({
   }
 });
 
+export const { updateOffset } = gifsSlice.actions
 export default gifsSlice.reducer;
