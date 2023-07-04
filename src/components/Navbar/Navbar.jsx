88dcddc81f51b33368fc998fdb9a03c374385226
BISWAP9 <116273370+BISWAP9@users.noreply.github.com>
2023-07-04 11:58:42 +0300

    fix: buttons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,34 @@
-import { NavLink } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import './Navbar.css'
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Button from '../Button/Button';
+import './Navbar.css';
 
 export default function Navbar() {
-  const { searchParams } = useSelector((store) => store.gifs)
+  const { searchParams } = useSelector(store => store.gifs);
 
   function handleActiveLink({ isActive }) {
-    return isActive ? 'nav__link nav__link_active' : 'nav__link'
+    return isActive ? 'nav__link nav__link_active' : 'nav__link';
   }
 
   return (
     <nav className="nav">
       <ul className="nav__links">
         <li>
-          <NavLink
-            to={`/gifs-application/${searchParams}`}
-            className={handleActiveLink}
-          >
-            Поиск
+          <NavLink to={`/gifs-application/${searchParams}`} className={handleActiveLink}>
+            <Button props="nav__button-link">Поиск</Button>
           </NavLink>
         </li>
         <li>
           <NavLink to="trendings" className={handleActiveLink}>
-            Тренды
+            <Button props="nav__button-link">Тренды</Button>
           </NavLink>
         </li>
         <li>
           <NavLink to="random" className={handleActiveLink}>
-            Случайный гиф
+            <Button props="nav__button-link">Рандом</Button>
           </NavLink>
         </li>
       </ul>
     </nav>
-  )
+  );
 }
