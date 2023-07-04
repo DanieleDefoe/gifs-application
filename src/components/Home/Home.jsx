2653f01fc9a7d80507de36166ff85ae11624ed16
BISWAP9 <116273370+BISWAP9@users.noreply.github.com>
2023-07-04 15:40:22 +0300

    feat: Error

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,41 +1,24 @@
-import { Fragment, useEffect } from 'react'
-import { useSearchParams } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import Form from '../Form/Form'
-import GifsContainer from '../GifsContainer/GifsContainer'
-import Loading from '../Loading/Loading'
-import Pagination from '../Pagination/Pagination'
-import { renderGifs } from '../../utils/utils'
-import './Home.css'
-import { getSearchGifs } from '../../store/gifsSlice'
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import Form from '../Form/Form';
+import GifsContainer from '../GifsContainer/GifsContainer';
+import Loading from '../Loading/Loading';
+import Pagination from '../Pagination/Pagination';
+import { renderGifs } from '../../utils/utils';
+import Error from '../Error/Error';
+import './Home.css';
 
 export default function Home() {
-  const { isLoading, data, loadedData, totalCount, offset } = useSelector(
-    (store) => store.gifs,
-  )
-  const [searchParams, setSearchParams] = useSearchParams()
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    const searchQueryParam = searchParams.get('search')
-    if (searchQueryParam) {
-      dispatch(getSearchGifs({ searchValue: searchQueryParam, offset: 0 }))
-    }
-  }, [])
+  const { isLoading, data, loadedData, totalCount, offset } = useSelector(store => store.gifs);
 
   return (
     <Fragment>
       <Form />
       {totalCount === 0 ? (
-        <h1>
-          Извините, по запросу "{searchParams.get('search')}" ничего не было
-          найдено.
-        </h1>
+        <Error />
       ) : (
         <>
-          {loadedData.length > 0 && (
-            <GifsContainer>{renderGifs(loadedData)}</GifsContainer>
-          )}
+          {loadedData.length > 0 && <GifsContainer>{renderGifs(loadedData)}</GifsContainer>}
           {isLoading ? (
             <Loading />
           ) : (
@@ -49,5 +32,5 @@ export default function Home() {
         </>
       )}
     </Fragment>
-  )
+  );
 }
