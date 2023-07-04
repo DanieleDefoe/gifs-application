848965cabcfb6b62255de5d629835c08326dffa4
Abuzar Mamedov <abuzar.mamedov@yandex.ru>
2023-07-04 17:37:55 +0300

    fix: minor chages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,11 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    "prettier",
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "prettier",
   ],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
