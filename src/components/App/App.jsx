9a4bc785c63e7d5b946fae80d6a61ec6dde0ade7
Abuzar Mamedov <abuzar.mamedov@yandex.ru>
2023-07-04 13:55:21 +0300

    feat: add error route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,24 +2,26 @@ import {
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
-  Route
-} from 'react-router-dom';
-import Layout from '../Layout/Layout';
-import Home from '../Home/Home';
-import Trendings from '../Trendings/Trendings';
-import Random from '../Random/Random';
-import './App.css';
+  Route,
+} from 'react-router-dom'
+import Layout from '../Layout/Layout'
+import Home from '../Home/Home'
+import Trendings from '../Trendings/Trendings'
+import Random from '../Random/Random'
+import Error from '../Error/Error'
+import './App.css'
 
 const routes = createRoutesFromElements(
   <Route path="/gifs-application/" element={<Layout />}>
     <Route index element={<Home />} />
     <Route path="trendings" element={<Trendings />} />
     <Route path="random" element={<Random />} />
-  </Route>
-);
+    <Route path="*" element={<Error />} />
+  </Route>,
+)
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes)
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />
 }
---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,5 @@
+import './Error.css'
+
+export default function Error() {
+  return <></>
+}
