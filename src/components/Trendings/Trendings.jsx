f875e4caa0af04580b9090274b9aa0e74c72b543
Abuzar Mamedov <abuzar.mamedov@yandex.ru>
2023-07-04 18:22:06 +0300

    fix: include trendings offset on mount

--- src/components/Trendings/Trendings.jsx
+++ src/components/Trendings/Trendings.jsx
@@ -1,22 +1,21 @@
-import { Fragment, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getTrendings } from "../../store/trendingsSlice";
-import { renderGifs } from "../../utils/utils";
-import GifsContainer from "../GifsContainer/GifsContainer";
-import Loading from "../Loading/Loading";
-import Gif from "../Gif/Gif";
-import Pagination from "../Pagination/Pagination";
-import "./Trendings.css";
+import { Fragment, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getTrendings } from '../../store/trendingsSlice'
+import { renderGifs } from '../../utils/utils'
+import GifsContainer from '../GifsContainer/GifsContainer'
+import Loading from '../Loading/Loading'
+import Pagination from '../Pagination/Pagination'
+import './Trendings.css'
 
 export default function Trendings() {
-  const { isLoading, data, loadedData } = useSelector(
-    (store) => store.trendings
-  );
-  const dispatch = useDispatch();
+  const { isLoading, data, loadedData, offset } = useSelector(
+    (store) => store.trendings,
+  )
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(getTrendings(0));
-  }, []);
+    dispatch(getTrendings({ offset }))
+  }, [])
 
   return (
     <Fragment>
@@ -32,5 +31,5 @@ export default function Trendings() {
         )
       )}
     </Fragment>
-  );
+  )
 }
