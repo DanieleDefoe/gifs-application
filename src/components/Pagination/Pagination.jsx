e2f871012fffdb3706b19e8a61dcc8a1ae40eded
Abuzar Mamedov <abuzar.mamedov@yandex.ru>
2023-07-04 13:46:43 +0300

    hotfix: add bg-image for arrow btn

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,31 +1,31 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux'
 import {
   getSearchGifs,
   updateOffset as updateGifsOffset,
-  updateLoadedGifs
-} from '../../store/gifsSlice';
+  updateLoadedGifs,
+} from '../../store/gifsSlice'
 import {
   getTrendings,
   updateOffset as updateTrendingsOffset,
-  updateLoadedTrendings
-} from '../../store/trendingsSlice';
-import Button from '../Button/Button';
+  updateLoadedTrendings,
+} from '../../store/trendingsSlice'
+import Button from '../Button/Button'
 
 export default function Pagination({ type }) {
-  const dispatch = useDispatch();
-  const { search: searchValue } = useSelector(store => store.form.values);
-  const { offset: gifsOffset } = useSelector(store => store.gifs);
-  const { offset: trendingsOffset } = useSelector(store => store.trendings);
+  const dispatch = useDispatch()
+  const { search: searchValue } = useSelector((store) => store.form.values)
+  const { offset: gifsOffset } = useSelector((store) => store.gifs)
+  const { offset: trendingsOffset } = useSelector((store) => store.trendings)
 
   function handlePagination() {
     if (type === 'search') {
-      dispatch(updateGifsOffset());
-      dispatch(getSearchGifs({ searchValue, offset: gifsOffset + 9 }));
-      dispatch(updateLoadedGifs());
+      dispatch(updateGifsOffset())
+      dispatch(getSearchGifs({ searchValue, offset: gifsOffset + 9 }))
+      dispatch(updateLoadedGifs())
     } else if (type === 'trendings') {
-      dispatch(updateTrendingsOffset());
-      dispatch(getTrendings({ offset: trendingsOffset + 9 }));
-      dispatch(updateLoadedTrendings());
+      dispatch(updateTrendingsOffset())
+      dispatch(getTrendings({ offset: trendingsOffset + 9 }))
+      dispatch(updateLoadedTrendings())
     }
   }
 
@@ -36,5 +36,5 @@ export default function Pagination({ type }) {
       onClick={handlePagination}
       props="button_type_show-more"
     />
-  );
+  )
 }
